ee4f97a30ccaaa8242e72208cc8874b5ec46b8a1
David Thomas <dave@davespace.co.uk>
2015-05-28 00:03:12 +0100

    Move masks data out of TheGreatEscape.c.

---
+++ libraries/TheGreatEscape/include/TheGreatEscape/Masks.h
@@ -0,0 +1,11 @@
+#ifndef MASKS_H
+#define MASKS_H
+
+#include <stdint.h>
+
+#include "TheGreatEscape/Types.h"
+
+const uint8_t *mask_pointers[30];
+const mask_t exterior_mask_data[58];
+
+#endif /* MASKS_H */
