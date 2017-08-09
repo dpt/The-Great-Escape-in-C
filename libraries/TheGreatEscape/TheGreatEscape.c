dpt/The-Great-Escape-in-C

commit 2c2442c13c43e861f84d001ba813e7b3e8ee4c8a

Giant checkpoint commit. This has all my recent changes all rolled together. I realise that this is bad form but I’ve left it too long between commits.

libraries/TheGreatEscape/include/TheGreatEscape/Types.h
@@ -165,12 +165,12 @@ enum vischar_flags
   vischar_BYTE1_CUTTING_WIRE           = 1 << 1, /* Player only? */
   vischar_BYTE1_PERSUE                 = 1 << 0, /* AI only? */
   vischar_BYTE1_BIT1                   = 1 << 1, /* AI only? */
-  vischar_BYTE1_BIT2                   = 1 << 2, /* 'Gone mad' (bribe) flag */
+  vischar_BYTE1_BIT2                   = 1 << 2, /* 'Gone mad' (bribed) flag */
   vischar_BYTE1_BIT6                   = 1 << 6, // affects scaling
   vischar_BYTE1_BIT7                   = 1 << 7,
 
-  vischar_BYTE2_MASK                   = 0x7F,
-  vischar_BYTE2_BIT7                   = 1 << 7,
+  vischar_BYTE2_MASK                   = 0x7F,   // target mask
+  vischar_BYTE2_BIT7                   = 1 << 7, // target mask
 
   vischar_BYTE7_MASK                   = 0x0F,
   vischar_BYTE7_MASK_HI                = 0xF0,
@@ -287,7 +287,7 @@ enum morale
 /**
  * Identifiers of map locations.
  */
-typedef enum location
+enum location
 {
   location_0E00                        = 0x0E00, /* used at exercise time (prior) */
   location_1000                        = 0x1000,
@@ -299,8 +299,12 @@ typedef enum location
   location_8502                        = 0x8502, /* used at bed time */
   location_8E04                        = 0x8E04, /* used at exercise time */
   location_9003                        = 0x9003, /* used at breakfast time */
-}
-location_t;
+};
+
+/**
+ * Holds a location.
+ */
+typedef uint16_t location_t;
 
 /**
  * Identifiers of map locations.
@@ -379,7 +383,7 @@ typedef struct movableitem
 {
   pos_t           pos;
   const sprite_t *spriteset;
-  uint8_t         b17;
+  uint8_t         b17; // flip flag? sprite set offset?
 }
 movableitem_t;
 
@@ -388,23 +392,23 @@ movableitem_t;
  */
 typedef struct vischar
 {
-  uint8_t       character; /* $8000 char index */
-  uint8_t       flags;     /* $8001 flags */
-  location_t    target;    /* $8002 target location */
-  tinypos_t     p04;       /* $8004 position */
-  uint8_t       b07;       /* $8007 more flags */
-  uint16_t      w08;       /* $8008 */ // only ever read in called_from_main_loop_9
-  uint16_t      w0A;       /* $800A */ // must be a pointer
-  uint8_t       b0C;       /* $800C */ // used with above?
-  uint8_t       b0D;       /* $800D movement */ // compared to flags?
-  uint8_t       b0E;       /* $800E walk/crawl flag */
-  movableitem_t mi;        /* $800F movable item (position, current character sprite set, b17) */
-  uint16_t      w18;       /* $8018 */ // coord
-  uint16_t      w1A;       /* $801A */ // coord
-  room_t        room;      /* $801C room index */
-  uint8_t       b1D;       /* $801D */ // no references
-  uint8_t       b1E;       /* $801E spotlight */
-  uint8_t       b1F;       /* $801F */ // compared to but never written? only used by sub_BAF7
+  uint8_t       character;    /* $8000 char index */
+  uint8_t       flags;        /* $8001 flags */
+  location_t    target;       /* $8002 target location */
+  tinypos_t     p04;          /* $8004 position */
+  uint8_t       b07;          /* $8007 more flags */
+  uint16_t      w08;          /* $8008 */ // only ever read in called_from_main_loop_9
+  uint16_t      w0A;          /* $800A */ // must be a pointer
+  uint8_t       b0C;          /* $800C */ // used with above?
+  uint8_t       b0D;          /* $800D movement */ // compared to flags?
+  uint8_t       b0E;          /* $800E walk/crawl flag */
+  movableitem_t mi;           /* $800F movable item (position, current character sprite set, b17) */
+  uint16_t      w18;          /* $8018 */ // coord
+  uint16_t      w1A;          /* $801A */ // coord
+  room_t        room;         /* $801C room index */
+  uint8_t       b1D;          /* $801D */ // can find no references. is this ever used?
+  uint8_t       width_bytes;  /* $801E copy of sprite width in bytes + 1 */
+  uint8_t       height;       /* $801F copy of sprite height in rows */
 }
 vischar_t;
 
@@ -464,7 +468,7 @@ typedef struct characterstruct
   character_t character;
   room_t      room;
   tinypos_t   pos;
-  uint8_t     t1, t2; // seems to be a copy of target
+  location_t  target;
 }
 characterstruct_t;
 
@@ -490,10 +494,10 @@ timedevent_t;
  */
 typedef struct itemstruct
 {
-  item_t    item;
-  room_t    room; // if has flags should be room_and_flags
-  tinypos_t pos;
-  uint8_t   t1, t2;
+  item_t     item;
+  room_t     room; // if has flags should be room_and_flags
+  tinypos_t  pos;
+  location_t target;
 }
 itemstruct_t;
 
@@ -519,10 +523,8 @@ typedef void (charevnt_handler_t)(tgestate_t  *state,
  */
 typedef struct doorpos
 {
-  uint8_t room_and_flags; // top 6 bits are room, bottom 2 are ?
-  // replace y,x,z with a tinypos_t?
-  uint8_t y, x;
-  uint8_t z;  // suspect: height
+  uint8_t   room_and_flags; // top 6 bits are room, bottom 2 are ?
+  tinypos_t pos;
 }
 doorpos_t;
 
@@ -551,7 +553,7 @@ byte_to_pointer_t;
  */
 typedef struct bounds
 {
-  uint8_t a, b, c, d; // TBD which of (a,b,c,d) is (x0,y0,x1,y1).
+  uint8_t a, b, c, d; // TBD which of (a,b,c,d) is (x0,x1,y0,y1).
 }
 bounds_t;
 
@@ -572,16 +574,7 @@ typedef struct default_item_location
 default_item_location_t;
 
 /**
- * Seven byte structures. Unknown yet. Likely indoor mask data.
- */
-typedef struct sevenbyte
-{
-  uint8_t a, b, c, d, e, f, g;
-}
-sevenbyte_t;
-
-/**
- * Eight byte structures. Unknown yet. Likely outdoor mask data.
+ * Eight byte structures. Unknown yet. Likely mask data.
  */
 typedef struct eightbyte
 {

include/TheGreatEscape/TheGreatEscape.h
@@ -40,7 +40,7 @@ extern "C"
  */
 typedef struct tgeconfig
 {
-  int width, height; // unused as yet
+  int width, height;
 }
 tgeconfig_t;
 

libraries/TheGreatEscape/Create.c
@@ -1,39 +1,326 @@
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "TheGreatEscape/State.h"
 
 #include "TheGreatEscape/TheGreatEscape.h"
 
+/**
+ * Initialise the state.
+ */
+static void tge_initialise(tgestate_t *state)
+{
+  /**
+   * $69AE: Movable items.
+   */
+  static const movableitem_t movable_items[movable_item__LIMIT] =
+  {
+    { { 62, 35, 16, }, &sprites[sprite_STOVE], 0 },
+    { { 55, 54, 14, }, &sprites[sprite_CRATE], 0 },
+    { { 62, 35, 16, }, &sprites[sprite_STOVE], 0 },
+  };
+
+  /**
+   * $7612: Character structures.
+   */
+  static const characterstruct_t character_structs[26] =
+  {
+    { character_0,  room_11_PAPERS,   {  46,  46, 24 }, 0x0003 },
+    { character_1,  room_0_OUTDOORS,  { 102,  68,  3 }, 0x0001 },
+    { character_2,  room_0_OUTDOORS,  {  68, 104,  3 }, 0x0201 },
+    { character_3,  room_16_CORRIDOR, {  46,  46, 24 }, 0x1303 },
+    { character_4,  room_0_OUTDOORS,  {  61, 103,  3 }, 0x0402 },
+    { character_5,  room_0_OUTDOORS,  { 106,  56, 13 }, 0x0000 },
+    { character_6,  room_0_OUTDOORS,  {  72,  94, 13 }, 0x0000 },
+    { character_7,  room_0_OUTDOORS,  {  72,  70, 13 }, 0x0000 },
+    { character_8,  room_0_OUTDOORS,  {  80,  46, 13 }, 0x0000 },
+    { character_9,  room_0_OUTDOORS,  { 108,  71, 21 }, 0x0004 },
+    { character_10, room_0_OUTDOORS,  {  92,  52,  3 }, 0x38FF },
+    { character_11, room_0_OUTDOORS,  { 109,  69,  3 }, 0x0000 },
+    { character_12, room_3_HUT2RIGHT, {  40,  60, 24 }, 0x0800 },
+    { character_13, room_2_HUT2LEFT,  {  36,  48, 24 }, 0x0800 },
+    { character_14, room_5_HUT3RIGHT, {  40,  60, 24 }, 0x1000 },
+    { character_15, room_5_HUT3RIGHT, {  36,  34, 24 }, 0x1000 },
+    { character_16, room_0_OUTDOORS,  {  68,  84,  1 }, 0x00FF },
+    { character_17, room_0_OUTDOORS,  {  68, 104,  1 }, 0x00FF },
+    { character_18, room_0_OUTDOORS,  { 102,  68,  1 }, 0x18FF },
+    { character_19, room_0_OUTDOORS,  {  88,  68,  1 }, 0x18FF },
+
+    { character_20, room_NONE,        {  52,  60, 24 }, 0x0800 }, // wake_up, breakfast_time
+    { character_21, room_NONE,        {  52,  44, 24 }, 0x0800 }, // wake_up, breakfast_time
+    { character_22, room_NONE,        {  52,  28, 24 }, 0x0800 }, // wake_up, breakfast_time
+
+    { character_23, room_NONE,        {  52,  60, 24 }, 0x1000 }, // wake_up, breakfast_time
+    { character_24, room_NONE,        {  52,  44, 24 }, 0x1000 }, // wake_up, breakfast_time
+    { character_25, room_NONE,        {  52,  28, 24 }, 0x1000 }, // wake_up, breakfast_time
+  };
+
+  /**
+   * $76C8: Item structs.
+   */
+  static const itemstruct_t item_structs[item__LIMIT] =
+  {
+    { item_WIRESNIPS,        room_NONE,        { 64, 32,  2 }, 0xF478 },
+    { item_SHOVEL,           room_9_CRATE,     { 62, 48,  0 }, 0xF27C },
+    { item_LOCKPICK,         room_10_LOCKPICK, { 73, 36, 16 }, 0xF077 },
+    { item_PAPERS,           room_11_PAPERS,   { 42, 58,  4 }, 0xF384 },
+    { item_TORCH,            room_14_TORCH,    { 34, 24,  2 }, 0xF67A },
+    { item_BRIBE,            room_NONE,        { 36, 44,  4 }, 0xF47E },
+    { item_UNIFORM,          room_15_UNIFORM,  { 44, 65, 16 }, 0xF187 },
+    { item_FOOD,             room_19_FOOD,     { 64, 48, 16 }, 0xF07E },
+    { item_POISON,           room_1_HUT1RIGHT, { 66, 52,  4 }, 0xF17C },
+    { item_RED_KEY,          room_22_REDKEY,   { 60, 42,  0 }, 0xF27B },
+    { item_YELLOW_KEY,       room_11_PAPERS,   { 28, 34,  0 }, 0xF881 },
+    { item_GREEN_KEY,        room_0_OUTDOORS,  { 74, 72,  0 }, 0x6E7A },
+    { item_RED_CROSS_PARCEL, room_NONE,        { 28, 50, 12 }, 0xF685 },
+    { item_RADIO,            room_18_RADIO,    { 36, 58,  8 }, 0xF485 },
+    { item_PURSE,            room_NONE,        { 36, 44,  4 }, 0xF47E },
+    { item_COMPASS,          room_NONE,        { 52, 28,  4 }, 0xF47E },
+  };
+
+  /**
+   * $EDD3: Game screen start addresses.
+   *
+   * Absolute addresses in the original code. These are now offsets.
+   */
+  static const uint16_t game_window_start_offsets[128] =
+  {
+    0x0047,
+    0x0147,
+    0x0247,
+    0x0347,
+    0x0447,
+    0x0547,
+    0x0647,
+    0x0747,
+    0x0067,
+    0x0167,
+    0x0267,
+    0x0367,
+    0x0467,
+    0x0567,
+    0x0667,
+    0x0767,
+    0x0087,
+    0x0187,
+    0x0287,
+    0x0387,
+    0x0487,
+    0x0587,
+    0x0687,
+    0x0787,
+    0x00A7,
+    0x01A7,
+    0x02A7,
+    0x03A7,
+    0x04A7,
+    0x05A7,
+    0x06A7,
+    0x07A7,
+    0x00C7,
+    0x01C7,
+    0x02C7,
+    0x03C7,
+    0x04C7,
+    0x05C7,
+    0x06C7,
+    0x07C7,
+    0x00E7,
+    0x01E7,
+    0x02E7,
+    0x03E7,
+    0x04E7,
+    0x05E7,
+    0x06E7,
+    0x07E7,
+    0x0807,
+    0x0907,
+    0x0A07,
+    0x0B07,
+    0x0C07,
+    0x0D07,
+    0x0E07,
+    0x0F07,
+    0x0827,
+    0x0927,
+    0x0A27,
+    0x0B27,
+    0x0C27,
+    0x0D27,
+    0x0E27,
+    0x0F27,
+    0x0847,
+    0x0947,
+    0x0A47,
+    0x0B47,
+    0x0C47,
+    0x0D47,
+    0x0E47,
+    0x0F47,
+    0x0867,
+    0x0967,
+    0x0A67,
+    0x0B67,
+    0x0C67,
+    0x0D67,
+    0x0E67,
+    0x0F67,
+    0x0887,
+    0x0987,
+    0x0A87,
+    0x0B87,
+    0x0C87,
+    0x0D87,
+    0x0E87,
+    0x0F87,
+    0x08A7,
+    0x09A7,
+    0x0AA7,
+    0x0BA7,
+    0x0CA7,
+    0x0DA7,
+    0x0EA7,
+    0x0FA7,
+    0x08C7,
+    0x09C7,
+    0x0AC7,
+    0x0BC7,
+    0x0CC7,
+    0x0DC7,
+    0x0EC7,
+    0x0FC7,
+    0x08E7,
+    0x09E7,
+    0x0AE7,
+    0x0BE7,
+    0x0CE7,
+    0x0DE7,
+    0x0EE7,
+    0x0FE7,
+    0x1007,
+    0x1107,
+    0x1207,
+    0x1307,
+    0x1407,
+    0x1507,
+    0x1607,
+    0x1707,
+    0x1027,
+    0x1127,
+    0x1227,
+    0x1327,
+    0x1427,
+    0x1527,
+    0x1627,
+    0x1727,
+  };
+
+  memcpy(state->movable_items, movable_items, sizeof(movable_items));
+
+  memcpy(state->character_structs,
+         character_structs,
+         sizeof(character_structs));
+
+  memcpy(state->item_structs, item_structs, sizeof(item_structs));
+
+  state->message_queue[message_queue_LENGTH - 1] = message_NONE;
+
+  state->moraleflag_screen_address =
+      &state->speccy->screen[0x5002 - SCREEN_START_ADDRESS];
+
+  // future: recalculate these
+  memcpy(state->game_window_start_offsets,
+         game_window_start_offsets,
+         sizeof(game_window_start_offsets));
+}
+
 tgestate_t *tge_create(ZXSpectrum_t *speccy, tgeconfig_t *config)
 {
-  tgestate_t *state;
+  tgestate_t  *state                     = NULL;
+  uint16_t    *game_window_start_offsets = NULL;
+  tileindex_t *tile_buf                  = NULL;
+  uint8_t     *window_buf                = NULL;
+  supertileindex_t   *map_buf                   = NULL;
 
   assert(config);
 
   if (!config)
-    return NULL;
+    goto failure;
+  
+  /* Allocate state structure. */
 
   state = malloc(sizeof(*state));
   if (state == NULL)
-    return NULL;
+    goto failure;
+  
+  /* Configure. */
+  
+  state->width      = config->width;
+  state->height     = config->height;
+  
+  /* This is the size of the game window only. */
+  state->columns    = 23;
+  state->rows       = 16;
+  
+  state->tb_columns = state->columns + 1;
+  state->tb_rows    = state->rows    + 1; // perhaps not
+  
+  /* This is held separately, rather than computed from columns and rows, as
+   * it's wider than I expected it to be. */
+  state->st_columns = 7;
+  state->st_rows    = 5;
+  
+  /* Allocate buffers. */
+  
+  game_window_start_offsets = malloc((size_t) (state->rows * 8) * sizeof(*game_window_start_offsets));
+  tile_buf                  = malloc((size_t) (state->tb_columns * state->tb_rows));
+  window_buf                = malloc((size_t) (state->columns * state->rows * 8));
+  map_buf                   = malloc((size_t) (state->st_columns * state->st_rows));
 
-  /* Initialise additional variables */
+  if (game_window_start_offsets == NULL ||
+      tile_buf                  == NULL ||
+      window_buf                == NULL ||
+      map_buf                   == NULL)
+    goto failure;
+  
+  state->game_window_start_offsets = game_window_start_offsets;
+  state->tile_buf                  = tile_buf;
+  state->window_buf                = window_buf;
+  state->map_buf                   = map_buf;
 
+  /* Initialise additional variables. */
+  
   state->speccy = speccy;
-
-  /* Initialise original game variables */
-
-  state->moraleflag_screen_address = &state->speccy->screen[0x5002 - SCREEN_START_ADDRESS];
+  
+  /* Initialise original game variables. */
+  
+  tge_initialise(state);
 
   return state;
+
+
+failure:
+
+  free(game_window_start_offsets);
+  free(map_buf);
+  free(window_buf);
+  free(tile_buf);
+  free(state);
+  
+  return NULL;
 }
 
 void tge_destroy(tgestate_t *state)
 {
   if (state == NULL)
     return;
+
+  free(state->game_window_start_offsets);
+  free(state->map_buf);
+  free(state->window_buf);
+  free(state->tile_buf);
 
   free(state);
 }

libraries/TheGreatEscape/Font.c
@@ -1,11 +1,10 @@
-/**
- * $A69E: Bitmap font definition.
- */
-
 #include "TheGreatEscape/Tiles.h"
 
 #include "TheGreatEscape/Font.h"
 
+/**
+ * $A69E: Bitmap font definition.
+ */
 const tile_t bitmap_font[38] =
 {
   { { 0x00, 0x7C, 0xFE, 0xEE, 0xEE, 0xEE, 0xFE, 0x7C } }, // 0 or O
@@ -56,10 +55,10 @@ const unsigned char ascii_to_font[256] =
 #define _ 38 // UNKNOWN
   _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,
   _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,
-  36,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _, 37,  _,
+ 36,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _, 37,  _,
   0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  _,  _,  _,  _,  _,  _,
-  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,  0,
-  25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,  _,  _,  _,  _,  _,
+ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,  0,
+ 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,  _,  _,  _,  _,  _,
   _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,
   _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,
   _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,  _,

libraries/TheGreatEscape/Map.c
@@ -1,10 +1,10 @@
-/**
- * $BCEE: Map super-tile refs. 54x34. Each byte represents a 32x32 tile.
- */
 
 #include "TheGreatEscape/Map.h"
 
-const maptile_t map_tiles[MAPX *MAPY] =
+/**
+ * $BCEE: Map super-tile refs. 54x34. Each byte represents a 32x32 tile.
+ */
+const supertileindex_t map[MAPX * MAPY] =
 {
   0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x5F, 0x33, 0x3C, 0x58, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60,
   0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x5F, 0x33, 0x34, 0x2E, 0x3D, 0x45, 0x3C, 0x58, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60, 0x60,

libraries/TheGreatEscape/Music.c
@@ -1,9 +1,8 @@
-/**
- * $????
- */
-
 #include "TheGreatEscape/Music.h"
 
+/**
+ * $F546
+ */
 const uint8_t music_channel0_data[] =
 {
   0x13, 0x00, 0x14, 0x00, 0x00, 0x15, 0x16, 0x00,
@@ -89,6 +88,9 @@ const uint8_t music_channel0_data[] =
   0xFF
 };
 
+/**
+ * $F7C7
+ */
 const uint8_t music_channel1_data[] =
 {
   0x0A, 0x00, 0x0C, 0x00, 0x00, 0x0E, 0x0F, 0x00,
@@ -174,6 +176,9 @@ const uint8_t music_channel1_data[] =
   0xFF
 };
 
+/**
+ * $FA48
+ */
 const uint16_t music_tuning_table[460] =
 {
   0xFEFE,

libraries/TheGreatEscape/RoomDefs.c
@@ -1,7 +1,3 @@
-/**
- * $6BAD: Room and tunnel definitions.
- */
-
 #include "TheGreatEscape/InteriorObjects.h"
 #include "TheGreatEscape/Rooms.h"
 
@@ -9,7 +5,10 @@
 
 /* Room defintions are not const as they are updated when objects are altered. */
 
-const roomdef_t rooms_and_tunnels[room__LIMIT] =
+/**
+ * $6BAD: Room and tunnel definitions.
+ */
+const roomdef_t *rooms_and_tunnels[room__LIMIT] =
 {
   /* Array of pointers to rooms (starting with room 1). */
   &roomdef_1_hut1_right[0],

commit 433dfa3d911791535f9494be291c107728f1a803

Fix: Sitting prisoners were not reappearing after breakfast.
character_sit_sleep_common was assigning route_WANDER to route->index, not zero, so the characters were still being moved around and ended up with an out-of-bounds position. They then refused to respawn.

Added route_HALT alias for route 0 and updated other references.

This was broken by 2c2442c13c43e861f84d001ba813e7b3e8ee4c8a (3-Apr-2014) when a `*HL = 0` became `*HL = room_NONE` (which is 255).

libraries/TheGreatEscape/TheGreatEscape.c
@@ -3469,7 +3469,7 @@ void character_sit_sleep_common(tgestate_t *state,
   // $8022 -> vischar[1]->route
   // others -> character_structs->route
 
-  route->index = route_WANDER; /* Choose random locations[]. */
+  route->index = route_HALT; /* Stand still. */
 
   if (state->room_index != room)
   {
@@ -3553,7 +3553,7 @@ void hero_sit_sleep_common(tgestate_t *state, uint8_t *pflag)
   *pflag = 255;
 
   /* Reset only the route index. */
-  state->vischars[0].route.index = 0; /* Stand still. */
+  state->vischars[0].route.index = route_HALT; /* Stand still. */
 
   /* Set hero position (x,y) to zero. */
   state->vischars[0].mi.pos.x = 0;
@@ -8194,7 +8194,7 @@ found_empty_slot:
   DEtarget = &vischar->target;
   HLroute  = &charstr->route;
 again:
-  if (HLroute->index != 0) /* if not stood still */
+  if (HLroute->index != route_HALT) /* if not stood still */
   {
     state->entered_move_characters = 0;
     // PUSH DE // -> vischar->pos
@@ -8608,7 +8608,7 @@ void move_characters(tgestate_t *state)
   // HL += 3; // point at charstr->route
 
   /* If standing still, return. */
-  if (charstr->route.index == 0) /* temp was A */
+  if (charstr->route.index == route_HALT) /* temp was A */
     // POP HL_pos
     return;
 
@@ -8950,7 +8950,7 @@ void character_event(tgestate_t *state, route_t *route)
 
   // POP route (HL)
 
-  route->index = 0; /* Stand still. */
+  route->index = route_HALT; /* Stand still. */
 }
 
 /**
@@ -9329,7 +9329,7 @@ bribed_visible:
     }
   }
 
-  if (vischar2->route.index == 0) /* Stand still */
+  if (vischar2->route.index == route_HALT) /* Stand still */
   {
     character_behaviour_set_input(state, vischar, 0 /* new_input */);
     return; // exit via
@@ -9584,7 +9584,7 @@ void target_reached(tgestate_t *state, vischar_t *vischar)
         food_discovered_counter = 255; /* food is poisoned */
       state->food_discovered_counter = food_discovered_counter;
 
-      vischar->route.index = 0; /* Stand still (ie. the dog is poisoned) */
+      vischar->route.index = route_HALT; /* Stand still (ie. the dog is poisoned) */
 
       character_behaviour_set_input(state, vischar, 0 /* new_input */);
       return;
@@ -9748,7 +9748,7 @@ do_character_event:
    */
 
   character_event(state, route);
-  if (route->index == 0) /* standing still */
+  if (route->index == route_HALT) /* standing still */
     return 0;
   else
     return get_target_assign_pos(state, vischar, route); // re-enter
@@ -10372,7 +10372,7 @@ next:
   vischar = &state->vischars[0];
   state->IY = &state->vischars[0];
   vischar->direction = direction_BOTTOM_LEFT;
-  state->vischars[0].route.index = 0; /* Stand still. */ // stores a byte, not a word
+  state->vischars[0].route.index = route_HALT; /* Stand still. */ // stores a byte, not a word
   transition(state, &solitary_pos);
 
   NEVER_RETURNS;

libraries/TheGreatEscape/include/TheGreatEscape/Types.h
@@ -362,7 +362,7 @@ typedef uint8_t eventtime_t;
  */
 typedef struct route
 {
-// FUTURE: Add route_STOP or route_HALT for 0
+#define route_HALT 0x00
 #define route_WANDER 0xFF
 #define route_REVERSED (1 << 7)
   uint8_t index; /** Route index as specified to get_route() or 0xFF for "wander". Set bit 7 to reverse the route. */