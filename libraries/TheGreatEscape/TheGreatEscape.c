dpt/The-Great-Escape-in-C

commit 7c3f96b88f0a1f131ee70973577c577ff7b08d12

mark_nearby_items: Rewrite - range checks look wrong.

libraries/TheGreatEscape/TheGreatEscape.c
@@ -11218,10 +11218,9 @@ void mark_nearby_items(tgestate_t *state)
   {
     const xy_t iso_pos = itemstruct->iso_pos; /* new */
 
-    /* Conv: Ranges adjusted. */ // but it still looks asymmetric...
     if ((itemstruct->room_and_flags & itemstruct_ROOM_MASK) == room &&
-        (iso_pos.x >= map_xy.x - 1 && iso_pos.x <= map_xy.x + (state->columns + 1) - 1) &&
-        (iso_pos.y >= map_xy.y     && iso_pos.y <= map_xy.y + state->rows))
+        (map_xy.x - 2 >= iso_pos.x && map_xy.x + (state->columns - 1) <= iso_pos.x) &&
+        (map_xy.y - 1 >= iso_pos.y && map_xy.y + (state->rows    - 1) <= iso_pos.y))
       itemstruct->room_and_flags |= itemstruct_ROOM_FLAG_NEARBY_6 | itemstruct_ROOM_FLAG_NEARBY_7; /* set */
     else
       itemstruct->room_and_flags &= ~(itemstruct_ROOM_FLAG_NEARBY_6 | itemstruct_ROOM_FLAG_NEARBY_7); /* reset */

commit e3fca6a22811eb024462446cb051204b4e9e3f64

Fix mark_nearby_items test having flipped signs.
This stopped items from appearing.
Fault introduced in 7c3f96b.

libraries/TheGreatEscape/TheGreatEscape.c
@@ -11165,8 +11165,8 @@ void mark_nearby_items(tgestate_t *state)
     const pos8_t isopos = itemstruct->isopos; /* new */
 
     if ((itemstruct->room_and_flags & itemstruct_ROOM_MASK) == room &&
-        (map_xy.x - 2 >= isopos.x && map_xy.x + (state->columns - 1) <= isopos.x) &&
-        (map_xy.y - 1 >= isopos.y && map_xy.y + (state->rows    - 1) <= isopos.y))
+        (map_xy.x - 2 <= isopos.x && map_xy.x + (state->columns - 1) >= isopos.x) &&
+        (map_xy.y - 1 <= isopos.y && map_xy.y + (state->rows    - 1) >= isopos.y))
       itemstruct->room_and_flags |= itemstruct_ROOM_FLAG_NEARBY_6 | itemstruct_ROOM_FLAG_NEARBY_7; /* set */
     else
       itemstruct->room_and_flags &= ~(itemstruct_ROOM_FLAG_NEARBY_6 | itemstruct_ROOM_FLAG_NEARBY_7); /* reset */