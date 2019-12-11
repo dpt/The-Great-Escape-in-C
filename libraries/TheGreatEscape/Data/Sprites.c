04c4c101c592274011dce3749a753ca1336512e4
David Thomas <dave@davespace.co.uk>
2019-12-11 20:34:08 +0000

    sprites[]: Document the corrections made to the sprites' heights.

--- libraries/TheGreatEscape/Data/Sprites.c
+++ libraries/TheGreatEscape/Data/Sprites.c
@@ -25,52 +25,57 @@
  * $CE22: Sprites: objects which can move.
  *
  * This includes STOVE, CRATE, PRISONER, CRAWL, DOG, GUARD and COMMANDANT.
+ *
+ * BUG FIX: The heights here have been corrected to match the actual bitmap and
+ *          mask data heights. In the original game this was not the case and
+ *          glitches could be seen because of it, e.g. when a guard dog is
+ *          running towards the bottom right part of the next frame can be seen.
  */
 const spritedef_t sprites[sprite__LIMIT] =
 {
-  { 3, 22, bitmap_stove,                            mask_stove                         }, // dims ok
-  { 4, 24, bitmap_crate,                            mask_crate                         }, // dims ok
+  { 3, 22, bitmap_stove,                            mask_stove                         },
+  { 4, 24, bitmap_crate,                            mask_crate                         },
 
-  { 3, 26, bitmap_prisoner_facing_top_left_1,       mask_various_facing_top_left_1     }, // ! sprite is 26 high
-  { 3, 27, bitmap_prisoner_facing_top_left_2,       mask_various_facing_top_left_2     }, // ! sprite is 27 high
-  { 3, 27, bitmap_prisoner_facing_top_left_3,       mask_various_facing_top_left_3     }, // ! sprite is 27 high
-  { 3, 27, bitmap_prisoner_facing_top_left_4,       mask_various_facing_top_left_4     }, // ! sprite is 27 high
-  { 3, 26, bitmap_prisoner_facing_bottom_right_1,   mask_various_facing_bottom_right_1 }, // ! sprite is 26 high
-  { 3, 28, bitmap_prisoner_facing_bottom_right_2,   mask_various_facing_bottom_right_2 }, // ! sprite is 28 high
-  { 3, 27, bitmap_prisoner_facing_bottom_right_3,   mask_various_facing_bottom_right_3 }, // ! sprite is 27 high
-  { 3, 27, bitmap_prisoner_facing_bottom_right_4,   mask_various_facing_bottom_right_4 }, // ! sprite is 27 high
+  { 3, 26, bitmap_prisoner_facing_top_left_1,       mask_various_facing_top_left_1     }, /* was 28 high */
+  { 3, 27, bitmap_prisoner_facing_top_left_2,       mask_various_facing_top_left_2     }, /* was 28 high */
+  { 3, 27, bitmap_prisoner_facing_top_left_3,       mask_various_facing_top_left_3     }, /* was 28 high */
+  { 3, 27, bitmap_prisoner_facing_top_left_4,       mask_various_facing_top_left_4     }, /* was 28 high */
+  { 3, 26, bitmap_prisoner_facing_bottom_right_1,   mask_various_facing_bottom_right_1 }, /* was 27 high */
+  { 3, 28, bitmap_prisoner_facing_bottom_right_2,   mask_various_facing_bottom_right_2 }, /* was 29 high */
+  { 3, 27, bitmap_prisoner_facing_bottom_right_3,   mask_various_facing_bottom_right_3 }, /* was 28 high */
+  { 3, 27, bitmap_prisoner_facing_bottom_right_4,   mask_various_facing_bottom_right_4 }, /* was 28 high */
 
-  { 4, 16, bitmap_crawl_facing_bottom_left_1,       mask_crawl_facing_bottom_left      }, // dims ok
-  { 4, 15, bitmap_crawl_facing_bottom_left_2,       mask_crawl_facing_bottom_left      }, // dims ok
-  { 4, 16, bitmap_crawl_facing_top_left_1,          mask_crawl_facing_top_left         }, // dims ok
-  { 4, 16, bitmap_crawl_facing_top_left_2,          mask_crawl_facing_top_left         }, // dims ok
+  { 4, 16, bitmap_crawl_facing_bottom_left_1,       mask_crawl_facing_bottom_left      },
+  { 4, 15, bitmap_crawl_facing_bottom_left_2,       mask_crawl_facing_bottom_left      },
+  { 4, 16, bitmap_crawl_facing_top_left_1,          mask_crawl_facing_top_left         },
+  { 4, 16, bitmap_crawl_facing_top_left_2,          mask_crawl_facing_top_left         },
 
-  { 4, 16, bitmap_dog_facing_top_left_1,            mask_dog_facing_top_left           }, // dims ok
-  { 4, 16, bitmap_dog_facing_top_left_2,            mask_dog_facing_top_left           }, // dims ok
-  { 4, 15, bitmap_dog_facing_top_left_3,            mask_dog_facing_top_left           }, // dims ok
-  { 4, 15, bitmap_dog_facing_top_left_4,            mask_dog_facing_top_left           }, // dims ok
-  { 4, 14, bitmap_dog_facing_bottom_right_1,        mask_dog_facing_bottom_right       }, // dims ok
-  { 4, 15, bitmap_dog_facing_bottom_right_2,        mask_dog_facing_bottom_right       }, // dims ok
-  { 4, 13, bitmap_dog_facing_bottom_right_3,        mask_dog_facing_bottom_right       }, // ! sprite is 13 high
-  { 4, 14, bitmap_dog_facing_bottom_right_4,        mask_dog_facing_bottom_right       }, // dims ok
+  { 4, 16, bitmap_dog_facing_top_left_1,            mask_dog_facing_top_left           },
+  { 4, 16, bitmap_dog_facing_top_left_2,            mask_dog_facing_top_left           },
+  { 4, 15, bitmap_dog_facing_top_left_3,            mask_dog_facing_top_left           },
+  { 4, 15, bitmap_dog_facing_top_left_4,            mask_dog_facing_top_left           },
+  { 4, 14, bitmap_dog_facing_bottom_right_1,        mask_dog_facing_bottom_right       },
+  { 4, 15, bitmap_dog_facing_bottom_right_2,        mask_dog_facing_bottom_right       },
+  { 4, 13, bitmap_dog_facing_bottom_right_3,        mask_dog_facing_bottom_right       }, /* was 15 high */
+  { 4, 14, bitmap_dog_facing_bottom_right_4,        mask_dog_facing_bottom_right       },
 
-  { 3, 27, bitmap_guard_facing_top_left_1,          mask_various_facing_top_left_1     }, // dims ok
-  { 3, 29, bitmap_guard_facing_top_left_2,          mask_various_facing_top_left_2     }, // dims ok
-  { 3, 27, bitmap_guard_facing_top_left_3,          mask_various_facing_top_left_3     }, // dims ok
-  { 3, 27, bitmap_guard_facing_top_left_4,          mask_various_facing_top_left_4     }, // dims ok
-  { 3, 29, bitmap_guard_facing_bottom_right_1,      mask_various_facing_bottom_right_1 }, // dims ok
-  { 3, 29, bitmap_guard_facing_bottom_right_2,      mask_various_facing_bottom_right_2 }, // dims ok
-  { 3, 28, bitmap_guard_facing_bottom_right_3,      mask_various_facing_bottom_right_3 }, // dims ok
-  { 3, 28, bitmap_guard_facing_bottom_right_4,      mask_various_facing_bottom_right_4 }, // dims ok
+  { 3, 27, bitmap_guard_facing_top_left_1,          mask_various_facing_top_left_1     },
+  { 3, 29, bitmap_guard_facing_top_left_2,          mask_various_facing_top_left_2     },
+  { 3, 27, bitmap_guard_facing_top_left_3,          mask_various_facing_top_left_3     },
+  { 3, 27, bitmap_guard_facing_top_left_4,          mask_various_facing_top_left_4     },
+  { 3, 29, bitmap_guard_facing_bottom_right_1,      mask_various_facing_bottom_right_1 },
+  { 3, 29, bitmap_guard_facing_bottom_right_2,      mask_various_facing_bottom_right_2 },
+  { 3, 28, bitmap_guard_facing_bottom_right_3,      mask_various_facing_bottom_right_3 },
+  { 3, 28, bitmap_guard_facing_bottom_right_4,      mask_various_facing_bottom_right_4 },
 
-  { 3, 28, bitmap_commandant_facing_top_left_1,     mask_various_facing_top_left_1     }, // dims ok
-  { 3, 30, bitmap_commandant_facing_top_left_2,     mask_various_facing_top_left_2     }, // dims ok
-  { 3, 29, bitmap_commandant_facing_top_left_3,     mask_various_facing_top_left_3     }, // dims ok
-  { 3, 29, bitmap_commandant_facing_top_left_4,     mask_various_facing_top_left_4     }, // dims ok
-  { 3, 27, bitmap_commandant_facing_bottom_right_1, mask_various_facing_bottom_right_1 }, // dims ok
-  { 3, 28, bitmap_commandant_facing_bottom_right_2, mask_various_facing_bottom_right_2 }, // dims ok
-  { 3, 27, bitmap_commandant_facing_bottom_right_3, mask_various_facing_bottom_right_3 }, // dims ok
-  { 3, 28, bitmap_commandant_facing_bottom_right_4, mask_various_facing_bottom_right_4 }, // dims ok
+  { 3, 28, bitmap_commandant_facing_top_left_1,     mask_various_facing_top_left_1     },
+  { 3, 30, bitmap_commandant_facing_top_left_2,     mask_various_facing_top_left_2     },
+  { 3, 29, bitmap_commandant_facing_top_left_3,     mask_various_facing_top_left_3     },
+  { 3, 29, bitmap_commandant_facing_top_left_4,     mask_various_facing_top_left_4     },
+  { 3, 27, bitmap_commandant_facing_bottom_right_1, mask_various_facing_bottom_right_1 },
+  { 3, 28, bitmap_commandant_facing_bottom_right_2, mask_various_facing_bottom_right_2 },
+  { 3, 27, bitmap_commandant_facing_bottom_right_3, mask_various_facing_bottom_right_3 },
+  { 3, 28, bitmap_commandant_facing_bottom_right_4, mask_various_facing_bottom_right_4 },
 };
 
 // vim: ts=8 sts=2 sw=2 et
