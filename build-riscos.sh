a5b936b87439322bb6b24baa171c3a0496f52527
David Thomas <dave@davespace.co.uk>
2021-01-12 22:59:07 +0000

    Add a CMake build which targets RISC OS using GCCSDK
    Note: Uses a temporary zerotape git repository address.

---
+++ build-riscos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+#
+# Build The Great Escape for RISC OS via GCCSDK
+#
+
+BUILDDIR=build-ro
+
+if ! command -v cmake &>/dev/null; then
+	echo "CMake could not be found"
+	exit
+fi
+
+if command -v ninja &>/dev/null; then
+	GENERATOR="-G Ninja"
+	PARALLEL=
+else
+	GENERATOR=
+	PARALLEL="--parallel 5" # wild guess
+fi
+
+mkdir -p $BUILDDIR && cd $BUILDDIR
+echo "Configuring..."
+cmake $GENERATOR -DCMAKE_TOOLCHAIN_FILE=../cmake/riscos.cmake ..
+echo "Building..."
+cmake --build . $PARALLEL
